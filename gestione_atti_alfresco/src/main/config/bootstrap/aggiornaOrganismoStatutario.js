aea0bbb8eb233c0c7b12bd5eded24359cd0b1158
federico.montinaro <federico.montinaro@localhost.localdomain>
2012-10-08 16:30:16 +0000

    I file js sono stati spostati nella sottocartella "script-editing"

--- gestione_atti_alfresco/src/main/config/bootstrap/aggiornaOrganismoStatutario.js
+++
@@ -1,8 +0,0 @@
-var organismiStatutari = space.getChildAssocsByType("crlatti:parere");
-var organismiStatutariAtto = new Array(organismiStatutari.length);
-for (var i=0; i<organismiStatutari.length; i++) {
-	organismiStatutariAtto[i] = organismiStatutari[i].properties["crlatti:organismoStatutarioParere"];
-}
-var attoNode = space.parent;
-attoNode.properties["crlatti:organismiStatutari"] = organismiStatutariAtto;
-attoNode.save();
--- gestione_atti_alfresco/src/main/config/bootstrap/anagraficaImport.js
+++
@@ -1,1 +0,0 @@
-crlImporter.executeImport();
