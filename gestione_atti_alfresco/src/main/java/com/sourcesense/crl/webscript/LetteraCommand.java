6cc7a65437db621682f955eeeb8e035e1c7d5a5a
federico.montinaro <federico.montinaro@localhost.localdomain>
2012-10-17 07:50:39 +0000

    Refactoring package com.sourcesense.crl.webscript

--- gestione_atti_alfresco/src/main/java/com/sourcesense/crl/webscript/LetteraCommand.java
+++
@@ -1,9 +0,0 @@
-package com.sourcesense.crl.webscript;
-
-import java.io.IOException;
-
-import org.alfresco.service.cmr.repository.NodeRef;
-
-public interface LetteraCommand {
-	byte[] generate(byte[] templateByteArray, NodeRef templateNodeRef)  throws IOException;
-}
