71f6f4af29b57f51ea7469060f56b4378dd252e3
federico.montinaro <federico.montinaro@localhost.localdomain>
2012-11-19 16:00:08 +0000

    Refactoring package lettere

--- gestione_atti_alfresco/src/main/java/com/sourcesense/crl/job/DataLoader.java
+++
@@ -1,9 +0,0 @@
-package com.sourcesense.crl.job;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public class DataLoader {
-
-    private static Log logger = LogFactory.getLog(DataLoader.class);
-}
