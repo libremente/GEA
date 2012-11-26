9b303c0143b8d44c23f2b510e5029203df87b652
alessandro.benedetti <alessandro.benedetti@localhost.localdomain>
2012-11-26 10:45:07 +0000

    aula reports added

---
+++ gestione_atti_alfresco/src/main/java/com/sourcesense/crl/webscript/report/aula/ReportAttiIstruttoriaCommand.java
@@ -0,0 +1,87 @@
+package com.sourcesense.crl.webscript.report.aula;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.web.bean.repository.Repository;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.json.JSONException;
+
+import com.sourcesense.crl.webscript.report.ReportBaseCommand;
+import com.sourcesense.crl.webscript.report.util.office.DocxManager;
+
+public class ReportAttiIstruttoriaCommand extends ReportBaseCommand {
+
+	@Override
+	public byte[] generate(byte[] templateByteArray, String json)
+			throws IOException {
+		ByteArrayOutputStream ostream = null;
+		try {
+			ByteArrayInputStream is = new ByteArrayInputStream(
+					templateByteArray);
+			DocxManager docxManager = new DocxManager(is);
+			this.initTipiAttoLucene(json);
+			// va filtrato lo stato preso in carico da aula
+			ResultSet queryRes = null;
+
+			for (int i = 0; i < tipiAttoLucene.size(); i++) {
+				queryRes = searchService.query(Repository.getStoreRef(),
+						SearchService.LANGUAGE_LUCENE, "TYPE:\""
+								+ tipiAttoLucene.get(i) +"\"");
+			}
+
+			// obtain resultSet Length and cycle on it to repeat template
+			XWPFDocument generatedDocument = docxManager.generateFromTemplate(
+					queryRes.length(), 4);
+			// convert to input stream
+			ByteArrayInputStream tempInputStream = saveTemp(generatedDocument);
+
+			XWPFDocument finalDocument = this.fillTemplate(tempInputStream,
+					queryRes);
+			ostream = new ByteArrayOutputStream();
+			finalDocument.write(ostream);
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ostream.toByteArray();
+
+	}
+
+	/**
+	 * ipotizzo field già presenti nella table, quindi inseriamo solo value
+	 * qui vanno inseriti nella table, presa dal template solo 8: tipo atto-
+	 * numero atto- abbinamenti - oggetto atto - iniziativa - firmatari - descrizione iniziativa - 
+	 * commissione referente - relatore - relazione scritta - note generali aula
+	 * 
+	 * @param finalDocStream
+	 * @param queryRes
+	 * @return
+	 * @throws IOException
+	 */
+	public XWPFDocument fillTemplate(ByteArrayInputStream finalDocStream,
+			ResultSet queryRes) throws IOException {
+		XWPFDocument document = new XWPFDocument(finalDocStream);
+		List<XWPFTable> tables = document.getTables();
+		for (int k = 0; k < queryRes.length(); k++) {
+			NodeRef currentNodeRef = queryRes.getNodeRef(k);
+			XWPFTable newTable = tables.get(k);
+			XWPFTableRow firstRow = newTable.getRow(0);
+
+			firstRow.getCell(0).setText("1x2");
+
+			XWPFTableRow secondRow = newTable.getRow(0);
+			secondRow.getCell(0).setText("1x1");
+			secondRow.getCell(0).setText("1x2");
+		}
+		return document;
+	}
+}
---
+++ gestione_atti_alfresco/src/main/java/com/sourcesense/crl/webscript/report/aula/ReportDCRCommand.java
@@ -0,0 +1,77 @@
+package com.sourcesense.crl.webscript.report.aula;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.web.bean.repository.Repository;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+
+import com.sourcesense.crl.webscript.report.ReportBaseCommand;
+import com.sourcesense.crl.webscript.report.util.office.DocxManager;
+
+public class ReportDCRCommand extends ReportBaseCommand {
+
+	@Override
+	public byte[] generate(byte[] templateByteArray, String json)
+			throws IOException {
+		ByteArrayOutputStream ostream = null;
+		ByteArrayInputStream is = new ByteArrayInputStream(templateByteArray);
+		DocxManager docxManager = new DocxManager(is);
+		// data seduta
+		String tipoAtto = "{" + CRL_ATTI_MODEL + "}" + "PDL";
+		ResultSet queryRes = null;
+
+		queryRes = searchService.query(Repository.getStoreRef(),
+				SearchService.LANGUAGE_LUCENE, "TYPE:\"" + tipoAtto + "\"");
+
+		// obtain resultSet Length and cycle on it to repeat template
+		XWPFDocument generatedDocument = docxManager.generateFromTemplate(
+				queryRes.length(), 4);
+		// convert to input stream
+		ByteArrayInputStream tempInputStream = saveTemp(generatedDocument);
+
+		XWPFDocument finalDocument = this.fillTemplate(tempInputStream,
+				queryRes);
+		ostream = new ByteArrayOutputStream();
+		finalDocument.write(ostream);
+		return ostream.toByteArray();
+
+	}
+
+	/**
+	 * ipotizzo field già presenti nella table, quindi inseriamo solo value qui
+	 * vanno inseriti nella table, presa dal template solo 8: tipo atto- numero
+	 * 
+	 * numero LCR - numero DCR - data seduta - oggetto atto - tipi atto - numero atto - 
+	 * emendata - commissione referente - numero LR - data LR - numero BURL - data BURL - note generali aula
+	 * 
+	 * @param finalDocStream
+	 * @param queryRes
+	 * @return
+	 * @throws IOException
+	 */
+	public XWPFDocument fillTemplate(ByteArrayInputStream finalDocStream,
+			ResultSet queryRes) throws IOException {
+		XWPFDocument document = new XWPFDocument(finalDocStream);
+		List<XWPFTable> tables = document.getTables();
+		for (int k = 0; k < queryRes.length(); k++) {
+			NodeRef currentNodeRef = queryRes.getNodeRef(k);
+			XWPFTable newTable = tables.get(k);
+			XWPFTableRow firstRow = newTable.getRow(0);
+
+			firstRow.getCell(0).setText("1x2");
+
+			XWPFTableRow secondRow = newTable.getRow(0);
+			secondRow.getCell(0).setText("1x1");
+			secondRow.getCell(0).setText("1x2");
+		}
+		return document;
+	}
+}
