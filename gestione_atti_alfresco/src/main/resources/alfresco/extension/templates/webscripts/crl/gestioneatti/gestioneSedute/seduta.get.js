16ce3dc00ad2f8ff9c794269459cc1a395c18151
piergiorgio.lucidi <piergiorgio.lucidi@pronetics.it>
2013-06-19 12:39:11 +0000

    fix per il webscript seduta: adesso viene utilizzata la provenienza per l'identificazione della seduta

--- gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/gestioneSedute/seduta.get.js
+++ gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/gestioneSedute/seduta.get.js
@@ -1,10 +1,17 @@
 <import resource="classpath:alfresco/extension/templates/webscripts/crl/gestioneatti/common.js">
 
+var provenienza = args.provenienza;
 var dataSeduta = args.dataSeduta;
 var legislatura = args.legislatura;
 
-var luceneQuery = 
-	    "TYPE:\"crlatti:sedutaODG\" " +
+var gestioneSedutePath = "/app:company_home"+
+"/cm:"+search.ISO9075Encode("CRL")+
+"/cm:"+search.ISO9075Encode("Gestione Atti")+
+"/cm:"+search.ISO9075Encode("Sedute")+
+"/cm:"+search.ISO9075Encode(provenienza);
+
+var luceneQuery = "PATH:\""+gestioneSedutePath+"//*\" "+
+	    "AND TYPE:\"crlatti:sedutaODG\" " +
 		"AND @crlatti\\:dataSedutaSedutaODG:\""+dataSeduta+"T00:00:00\" " +
 		"AND @crlatti\\:legislaturaSedutaODG:\""+legislatura+"\"";
 
@@ -16,6 +23,6 @@ if(sedutaResults!=null && sedutaResults.length>0){
 	model.seduta = seduta;
 } else {
 	status.code = 404;
-	status.message = "Seduta con data "+dataSeduta+" e legislatura "+legislatura+" non trovata";
+	status.message = "Seduta non trovata con provenienza "+provenienza+" | Data "+dataSeduta+" | Legislatura "+legislatura;
 	status.redirect = true;
 }
