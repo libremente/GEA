c89aa9bb95c89f5d41dbd4cd3fc253528bde4f79
Stefano Conti <stefano.conti@pronetics.it>
2013-06-21 09:46:30 +0000

    fix query firmatari storici

--- gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/anagrafica/firmatariAll.get.js
+++ gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/anagrafica/firmatariAll.get.js
@@ -3,7 +3,7 @@ var firmatariStoriciPath = "/app:company_home" +
 	"/cm:"+search.ISO9075Encode("CRL") +
 	"/cm:"+search.ISO9075Encode("Gestione Atti") +
 	"/cm:"+search.ISO9075Encode("Anagrafica") +
-	"/cm:"+search.ISO9075Encode("ConsiglieriStorici") + "/*";
+	"/cm:"+search.ISO9075Encode("ConsiglieriStorici");
 var luceneQuery = "PATH:\""+firmatariStoriciPath+"/*\" AND @crlatti\\:legislaturaConsigliereAnagrafica:\""+legislatura+"\"";
 var firmatariStoriciResults = search.luceneSearch(luceneQuery);
 model.firmatari = firmatariStoriciResults;
