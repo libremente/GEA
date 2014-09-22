a72da6a70c9699a84f961f5ab77058aaa6463f12
piergiorgio.lucidi <piergiorgio.lucidi@pronetics.it>
2014-09-22 13:12:16 +0000

    aggiornato il WebScript per la query degli atti indirizzo: adesso richiede il tipo atto

--- gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/attiIndirizzo/attiIndirizzo.get.js
+++ gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/attiIndirizzo/attiIndirizzo.get.js
@@ -2,13 +2,23 @@
 
 var legislaturaCorrente = getLegislaturaCorrente();
 
+var tipo = args.tipo;
+
+if(tipo==null 
+		|| tipo==undefined
+		|| tipo==""){
+	status.code = 400;
+	status.message = "Parametro tipo non presente nella richiesta";
+	status.redirect = true;
+}
+
 var gestioneAttiPath = "/app:company_home"+
 "/cm:"+search.ISO9075Encode("CRL")+
 "/cm:"+search.ISO9075Encode("Gestione Atti");
 
 var legislaturaRepositoryPath = gestioneAttiPath + "/cm:"+search.ISO9075Encode(legislaturaCorrente);
 var attoIndirizzoLuceneQuery = "PATH:\""+legislaturaRepositoryPath+"//*\"";
-attoIndirizzoLuceneQuery += " AND TYPE:\"crlatti:attoIndirizzo\"";
+attoIndirizzoLuceneQuery += " AND TYPE:\"crlatti:attoIndirizzo\" AND @crlatti\\:tipoAttoIndirizzo:\""+tipo+"\"";
 
 var attoIndirizzoResults = search.luceneSearch(attoIndirizzoLuceneQuery);
 
