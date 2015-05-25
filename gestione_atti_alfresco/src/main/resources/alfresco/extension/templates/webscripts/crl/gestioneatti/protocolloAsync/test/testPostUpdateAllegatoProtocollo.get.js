90ea4f162985555a36917ec030656ef09ae8f70b
matteo.grolla <matteo.grolla@pronetics.it>
2015-05-25 14:20:00 +0000

    Fix: limitato lo scope della query per evitare il timeout

--- gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/protocolloAsync/test/testPostUpdateAllegatoProtocollo.get.js
+++ gestione_atti_alfresco/src/main/resources/alfresco/extension/templates/webscripts/crl/gestioneatti/protocolloAsync/test/testPostUpdateAllegatoProtocollo.get.js
@@ -1,3 +1,3 @@
-var luceneQueryAtti = "TYPE:\"crlatti:allegato\" TYPE:\"crlatti:testo\"";
+var luceneQueryAtti = "TYPE:\"crlatti:allegato\" TYPE:\"crlatti:testo\" @cm\\:modified:[2015-01-01T00:00:00 TO NOW]";
 var attiResults = search.luceneSearch(luceneQueryAtti);
 model.attiResults = attiResults;
