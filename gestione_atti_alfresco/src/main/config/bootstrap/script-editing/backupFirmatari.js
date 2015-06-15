62207bee55f9d7fc7f03f36f27903f170ae4b3d6
matteo.grolla <matteo.grolla@pronetics.it>
2015-06-15 16:33:07 +0000

    working backupFirmatari.js

--- gestione_atti_alfresco/src/main/config/bootstrap/script-editing/backupFirmatari.js
+++ gestione_atti_alfresco/src/main/config/bootstrap/script-editing/backupFirmatari.js
@@ -1,33 +1,48 @@
-startTs = Date.now();
+exit = false;
 
 function backup(folder){
-    logger.log("backup: "+folder.displayPath);
-    data = [];
+    var path = folder.displayPath.split("/");
+    var level = path.length;
+    var fullPath = folder.displayPath+"/"+folder.name;
+    logger.log("backup: "+fullPath+" level: "+level);
+    var data = [];
 
-    if (Date.now() - startTs > 10000){
+    if (exit){
         return data;
     }
 
     if (folder.isSubType("crlatti:atto")){
         var firmatari = folder.properties["crlatti:firmatari"];
         if (firmatari){
-            data.push('"'+folder.displayPath+'" : "' + firmatari + '"');
+            data.push('"'+fullPath+'" : "' + firmatari + '"');
+            logger.log(folder.name+" : "+firmatari);
+        } else {
+            logger.log(folder.name+" empty");
         }
-    } else {
+
+    } else if (level <= 7 ) {   //|| (level == 8 && fullPath.indexOf("AttiIndirizzo")!=-1 )
         var thisChildren = folder.children;
-        for (i = 0; i< thisChildren.length; i++){
+        if (!thisChildren){
+            thisChildren = [];
+        }
+        logger.log(folder.name+" "+thisChildren.length+" children");
+        for (var i = 0; i< thisChildren.length; i++){
             var child = thisChildren[i];
             var thisData = backup(child);
-            data.concat(thisData);
+            data = data.concat(thisData);
         }
     }
     return data;
 }
 
 backupData = backup(space);
-file = companyhome.createFile("backupFirmatari.json");
+var file = companyhome.childByNamePath("backupFirmatari.json");
+if (!file){
+    file = companyhome.createFile("backupFirmatari.json");
+}
+
 file.content = '{\n';
-var body = data.join(",\n");
+var body = backupData.join(",\n");
 file.content += body;
 file.content += '}\n';
 file.save();
